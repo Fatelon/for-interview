ae884ea9e4e7eac5dacd44fbcfa004be4ce79b73
Sergei <fatelon@gmail.com>
2023-08-20 17:12:51 +0700

    feat(test-lib): link the lib to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TestLibModule } from 'test-lib';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
