1ca04e6dc73c705e14aa467195d408a9ac4e95ae
Sergei <fatelon@gmail.com>
2023-08-20 21:54:27 +0700

    feat(home): prepare simple test form group

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { SimpleTextComponent } from 'test-lib';
+
+
+interface ISimpleTestForm {
+  userName: FormControl<string | null>;
+  useremail: FormControl<string | null>;
+}
 
 @Component({
-  selector: 'fi-home',
   standalone: true,
-  imports: [CommonModule],
+  selector: 'fi-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ReactiveFormsModule, SimpleTextComponent,
+  ],
 })
 export default class HomeComponent {
+  protected readonly formBuilder: FormBuilder = inject(FormBuilder);
 
+  protected simpleFormGroup: FormGroup<ISimpleTestForm> = this.formBuilder.group({
+    userName: new FormControl<string>('', [Validators.required]),
+    useremail: new FormControl<string>('', [Validators.required, Validators.email]),
+  });
 }
